4bf68431a83e0960d956fa09d40d4b82776c22f7
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-05 19:56:56 +1100

    complete excercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < b) {
+        smallerNumber = a;
+        biggerNumber = b;
+    } else if (b < a) {
+        biggerNumber = a;
+        smallerNumber = b;
+    }
 
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR"
+    } if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+
+    total = 0
+    for (i = smallerNumber; i <= biggerNumber; i++) {
+        total += i
+    }
+
+    return total
 };
 
 // Do not edit below this line

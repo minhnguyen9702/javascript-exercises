c475cb60e7a669fb9bfc141ff6f1596055e2c364
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-05 19:47:17 +1100

    complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...theArgs) {
+    for (let i = 0; i <= arr.length - 1; i++) {
+        let j = 0;
+        while (j <= theArgs.length - 1) {
+            if (arr[i] === theArgs[j]) {
+                arr.splice(i, 1);
+                j = 0
+            } else {
+                j++
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line

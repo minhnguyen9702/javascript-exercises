be51cac09f6e2e139bbeaaaed17d5938db1e863d
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-05 18:39:32 +1100

    complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    result = ""
+    if (num < 0) {
+        return "ERROR"
+    }
 
+    i = 0
+    while (i < num) {
+        result += string
+        i++
+    }
+
+    return result
 };
 
 // Do not edit below this line

a05b99616576cfdbc5e84cd6781f88d866ae5d03
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-13 21:49:09 +1100

    complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(ls) {
+  total = 0
+	for (element in ls)
+    total += ls[element]
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(ls) {
+  total = 1
+	for (element in ls)
+    total *= ls[element]
+  return total
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let result = num ** power
+	return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 1) {
+    return num
+  } if (num == 0) {
+    return 1
+  } else {
+    return num*factorial(num-1)
+  }
 };
 
 // Do not edit below this line

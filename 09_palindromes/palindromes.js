ce2c1a17c96cbdad9583d5719de3a86e357a09f4
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-14 19:56:52 +1100

    complete exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/\W|_/g, '');
+    if (string.length == 0 || string.length == 1) {
+        return true
+    } if (string[0] == string[string.length - 1]) {
+        string = string.slice(1, -1)
+        return palindromes(string)
+    } else if (string[0] != string[string.length - 1]) {
+        return false
+    }
 
 };
 

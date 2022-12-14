f82faf56243f884411c177f3bbbbefa612fb5f98
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-14 20:28:42 +1100

    complete exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const sortedBooks = books.map(function(book) {
+        return book.title
+    })
+    return sortedBooks
 };
 
 // Do not edit below this line

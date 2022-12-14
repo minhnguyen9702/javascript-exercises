1d7021449cf1eeeb838c7830b18f75deb7aa1e33
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-14 21:07:31 +1100

    complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge = 0
+    let oldestPerson
+    for (person in people) {
+        if (people[person].yearOfDeath === undefined) {
+            people[person].yearOfDeath = new Date().getFullYear()
+        } if ((people[person].yearOfDeath - people[person].yearOfBirth) > oldestAge) {
+            oldestAge = (people[person].yearOfDeath - people[person].yearOfBirth)
+            oldestPerson = people[person]
+        }
+    }
+    return oldestPerson
 };
 
 // Do not edit below this line
